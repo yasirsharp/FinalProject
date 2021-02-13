a2c6144eab4c3bc3b2e4a6dde73bf120e9be181d
Yasir Demirci <yasirdemirci134@gmail.com>
2021-02-13 22:50:47 +0300

    13.02.2021 22:50

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -35,10 +35,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MainTenanceTime);
-            }
+            //if (DateTime.Now.Hour == 22)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MainTenanceTime);
+            //}
 
             return new SuccesDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductListed);
         }
